8d16205ccb054d396486947d6b95e19ccfa34868
Tarel-Winner <tarel.tarik23@gmail.com>
2024-04-13 03:49:54 +0300

    server commit

---
+++ api/server.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store *db.Store
+	router *gin.Engine
+}
+func errorResponse (err error) gin.H {
+	return gin.H{"error":err.Error()}
+}
+
+func NewServer(store *db.Store)*Server {
+	server:=&Server{store:store}
+	router:=gin.Default()
+    router.POST("/accounts",server.createAccount)
+
+
+	server.router=router
+	return server
+}
